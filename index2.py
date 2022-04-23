94fdd4fd3d7a1df21608fbd8cdfe71cd607bead1
dickywahyudi17 <104243162+dickywahyudi17@users.noreply.github.com>
2022-04-23 12:19:22 +0700

    Add files via upload

---
+++ index2.py
@@ -0,0 +1,18 @@
+x=3
+y=2.5
+
+pangkat = x**y
+bagi = x/y
+tambah_5 = x+5
+kali = x*y
+
+if 5 <= kali <= bagi <= 8:
+print('x * y = '+str(kali))
+print('hasil lebih kecil dari 10')
+
+if kali == 7.5:
+print('hasil kali = 7.5')
+
+print('x bagi y= '+str(bagi))
+print('x pangkat y = 'str(pangkat))
+print('sekarang nilai x = 'str(x))
